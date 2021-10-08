79c5884223dd7b54407f688dc0665a9979921029
Kishan Parekh <kishan_parekh@outlook.com>
2021-10-08 14:14:41 -0400

    Adding dist to Git repo

--- run.bat
+++ run.bat
@@ -5,9 +5,6 @@ cd dist/highest_profit
 REM Verify a highest_profit.exe file exists
 if not exist highest_profit.exe goto failure
 
-REM  obtain file
-curl -O -s https://gist.githubusercontent.com/bobbae/b4eec5b5cb0263e7e3e63a6806d045f2/raw/279b794a834a62dc108fc843a72c94c49361b501/data.csv
-dir | findstr "data.csv"
 @echo:
 REM run the standalone file
 highest_profit.exe
